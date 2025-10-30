21008c36ce75a1ca81d6df4933f528ab456e27bb
thegears <semh.1362@gmail.com>
2025-10-30 23:23:40 +0000

    updated build script

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const withNextIntl = createNextIntlPlugin("./i18n.ts");
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
 };
 
 export default withNextIntl(nextConfig);
